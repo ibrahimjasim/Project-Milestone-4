2aed126a7f4d172b47d5c6100f1e1e0d091850c9
ibrahimjasim <>
2023-10-13 17:47:08 +0000

    Deploy to Heroku

--- sooq/settings.py
+++ sooq/settings.py
@@ -138,6 +138,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
