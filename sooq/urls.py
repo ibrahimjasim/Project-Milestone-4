d20385af1b1af1e2539d4c1cec8038e57de03b09
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-02-10 09:42:27 +0000

    Edit the sooq urls.py  to make it pep8 compatible

--- sooq/urls.py
+++ sooq/urls.py
@@ -10,5 +10,4 @@ urlpatterns = [
     path('dashboard/', include('dashboard.urls')),
     path('inbox/', include('conversation.urls')),
     path('admin/', admin.site.urls),
-    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
