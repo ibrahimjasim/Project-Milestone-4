700b0c9d8e2fc3d1fcb7c1d82c6bb26df1ed51a7
Ibrahim Jasim <alazawy.m91@gmail.com>
2024-02-10 09:48:24 +0000

    Made the core viwes.py pep8 compatible

--- core/views.py
+++ core/views.py
@@ -4,14 +4,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
 
-
-
-
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
-
-
 
     return render(request, 'core/index.html', {
 
@@ -20,19 +15,13 @@ def index(request):
     })
 
 
-
 def contact(request):
     return render(request, 'core/contact.html')
-
-
-
 
 
 def signup(request):
     if request.method == 'POST':
         form = SignupForm(request.POST)
-
-
 
         if form.is_valid():
             form.save()
